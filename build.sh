757c1f6495bdf6443a10fa901adf7ddcca673eb4
redphx <96280+redphx@users.noreply.github.com>
2026-01-17 21:03:43 +0700

    feat: clean before building

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-find ./src -type f \( -name '*.o' -o -name '*.moc' \) -delete
-
+# Remove old files
+docker run --volume="$PWD:$PWD" --user="$(id -u):$(id -g)" --workdir="$PWD" --env=HOME --entrypoint=make --rm -it ghcr.io/pgaskin/nickeltc:1.0 clean
+# Build plugin
 docker run --volume="$PWD:$PWD" --user="$(id -u):$(id -g)" --workdir="$PWD" --env=HOME --entrypoint=make --rm -it ghcr.io/pgaskin/nickeltc:1.0 all koboroot
